ec5a5e0842ea2504dcbc789a483388f8a023b86d
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-02-22 12:48:44 -0500

    mi primer commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("bienvenidos a este programa podra saber algoritmo que permita calcular el producto entre un número entero a y  un número entero número entero c dado, también deberá calcular el doble del número a ")
+a=int(input("numero a: "))
+c=int(input("numero c: "))
+prod= a*c
+print("El producto es:" , prod)
+print("2xa= ", a*2)

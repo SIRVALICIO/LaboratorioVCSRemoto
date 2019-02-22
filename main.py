f0c9c2bc3476a4476e3eaf9dcb0ac1afc206bafa
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-02-22 13:52:11 -0500

    mi primer modificacion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-print("bienvenidos a este programa podra saber algoritmo que permita calcular el producto entre un número entero a y  un número entero número entero c dado, también deberá calcular el doble del número a ")
-a=int(input("numero a: "))
-c=int(input("numero c: "))
-prod= a*c
-print("El producto es:" , prod)
-print("2xa= ", a*2)
+b=int(input("numer b: "))
+d=float(input("numero d: "))
+print("el cuadrado es: ", b**2)
+print("la raiz es: ", d**(1/2))
